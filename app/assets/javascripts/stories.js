e6383c8826381345a358c153e0d0c9af0a7d5ed0
Lena <fatmeow@gmail.com>
2018-03-31 13:04:15 -0400

    Fix infinite scrolling, delete unecessary lines

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -1,15 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  var contentDiv = document.querySelector('#content'),
-      viewmoreP = document.querySelector('#view-more'),
-      lastDiv = document.querySelector('div:last-child'),
-      lastDivOffset = lastDiv.offsetTop + lastDiv.clientHeight,
-      pageOffset =  window.pageYOffset + window.innerHeight,
+  var viewmoreP = document.querySelector('#view-more'),
       isLoading = false;
 
   var nextPage = function() {
-    if (pageOffset > lastDivOffset - 20) {
-
       console.log("scrolled down")
       url = viewmoreP.querySelector('a').href
 
@@ -26,15 +20,18 @@ document.addEventListener('DOMContentLoaded', function () {
           isLoading = false;
         }
       )
-    // var newDiv = document.createElement("div");
-    // newDiv.innerHTML = "my awesome new div";
-    // document.getElementById("scroll-content").appendChild(newDiv);
-    // nextPage();
-    }
+
   }
 
   document.addEventListener("scroll", function (event) {
-      nextPage();
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight){
+      nextPage()
+    }
   })
+
+  viewmoreP.querySelector('a').addEventListener('click', function(e) {
+    nextPage();
+    e.preventDefault()
+  });
 
 })
